73ebbbf1ed71941ae08fc47e15b4f56b70cd9973
Simon Kim <spamhere@kaoma.net>
2023-11-12 07:47:24 +0100

    Initial Commit

---
+++ LoremPicsum/LoremPicsumApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoremPicsumApp.swift
+//  LoremPicsum
+//
+//  Created by Simon Kim on 11/12/23.
+//
+
+import SwiftUI
+
+@main
+struct LoremPicsumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
